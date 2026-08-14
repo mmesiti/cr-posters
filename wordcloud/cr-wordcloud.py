ae62b6edc458e0c480edde3baed94da30d3f511c
Michele Mesiti <michele.mesiti@kit.edu>
2026-08-14 14:41:41 +0200

    wordcloud: cr logo mask included (WIP)

--- wordcloud/cr-wordcloud.py
+++ wordcloud/cr-wordcloud.py
@@ -1,8 +1,67 @@
 #!/usr/bin/env python3
 from wordcloud import WordCloud
 from matplotlib import pyplot as plt
+from PIL import Image, ImageOps
+import numpy as np
+from sys import argv
 
-wc = WordCloud(background_color="white", max_words=100)
+def main():
+    logo_path, output_path = get_args()
+    logo, mask = get_logo_and_mask(logo_path)
+    wc = get_wordcloud(mask=mask, frequencies=frequencies)
+    recombined = recombine(wc,logo,mask)
+    
+    visualize(logo,mask,recombined)
+    save(recombined, output_path)
+
+##
+def get_args():
+    try:
+        logo_path = argv[1] 
+    except:
+        logo_path = "./cr-mask-negative-fullpage.png"
+
+    try:
+        output_path = argv[2] 
+    except:
+        output_path = "cr-wordcloud.png"
+
+    return logo_path, output_path
+
+
+def get_logo_and_mask(logo_path):
+    logo = Image.open(logo_path)
+    mask = 255*(np.array(
+            ImageOps.grayscale(logo)) != 255)
+
+    return logo, mask 
+
+def get_wordcloud(mask,frequencies):
+    wc = WordCloud(background_color="white", max_words=100, mask=mask)
+    wc.generate_from_frequencies(frequencies)
+    return wc
+
+def recombine(wc,logo,mask):
+    logo_masked = np.einsum("ij,ijk->ijk",mask,logo)[:,:,:3]/255
+    wc_masked = np.einsum("ij,ijk->ijk",255-mask,wc)/255
+
+    return np.array( logo_masked + wc_masked, dtype=np.uint8)
+
+def visualize(*figs):
+    for fig in figs:
+        plt.figure()
+        plt.imshow(fig, interpolation="bilinear")
+        plt.axis("off")
+
+    plt.show()
+
+def save(fig,output_path):
+    plt.imshow(fig,interpolation="bilinear")
+    plt.axis("off")
+    plt.savefig(output_path)
+
+###
+
 frequencies = {"git": 10, 
                "git commit": 1,
                "git push": 1,
@@ -51,7 +110,8 @@ frequencies = {"git": 10,
                "TDD": 1,
                }
 
-wc.generate_from_frequencies(frequencies)
-plt.imshow(wc)
-plt.axis("off")
-plt.savefig("cr-wordcloud.png")
+
+if __name__ == "__main__":
+    main()
+
+
