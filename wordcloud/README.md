3f565c3cbe417ac84dd4c141762fe837ab6ba88c
Michele Mesiti <michele.mesiti@kit.edu>
2026-08-21 17:36:19 +0200

    Tweaks to wordcloud (default horizontal, argparse)

---
+++ wordcloud/README.md
@@ -0,0 +1,7 @@
+# Getting vertical Logos (masks)  
+
+Use *ImageMagick*:
+```bash
+ magick cr-mask-negative-small.png -rotate 90 cr-mask-negative-small-vertical.png 
+ magick cr-mask-negative-fullpage.png -rotate 90 cr-mask-negative-fullpage-vertical.png 
+```
--- wordcloud/cr-wordcloud.py
+++ wordcloud/cr-wordcloud.py
@@ -4,11 +4,12 @@ from matplotlib import pyplot as plt
 from PIL import Image, ImageOps
 import numpy as np
 from sys import argv
+import argparse as ap
 
 def main():
     logo_path, output_path = get_args()
     logo, mask = get_logo_and_mask(logo_path)
-    wc = get_wordcloud(mask=mask, frequencies=frequencies)
+    wc = get_wordcloud(mask=mask, weights=weights)
     recombined = recombine(wc,logo,mask)
     
     visualize(logo,mask,recombined)
@@ -16,17 +17,20 @@ def main():
 
 ##
 def get_args():
-    try:
-        logo_path = argv[1] 
-    except:
-        logo_path = "./cr-mask-negative-fullpage.png"
+    parser = ap.ArgumentParser("""
+    A tool to generate CodeRefinery wordcloud starting from the logo,
+    and a list of words with their weight.
+    """)
+    parser.add_argument("--logo-path",
+                       help="Path of the png logo",
+                       default="./cr-mask-negative-fullpage.png")
+    parser.add_argument("--output",
+                        help="Output file",
+                        default="cr-wordcloud.png")
 
-    try:
-        output_path = argv[2] 
-    except:
-        output_path = "cr-wordcloud.png"
+    args = parser.parse_args()
 
-    return logo_path, output_path
+    return args.logo_path, args.output
 
 
 def get_logo_and_mask(logo_path):
@@ -36,9 +40,9 @@ def get_logo_and_mask(logo_path):
 
     return logo, mask 
 
-def get_wordcloud(mask,frequencies):
+def get_wordcloud(mask,weights):
     wc = WordCloud(background_color="white", max_words=100, mask=mask)
-    wc.generate_from_frequencies(frequencies)
+    wc.generate_from_frequencies(weights)
     return wc
 
 def recombine(wc,logo,mask):
@@ -62,53 +66,53 @@ def save(fig,output_path):
 
 ###
 
-frequencies = {"git": 10, 
-               "git commit": 1,
-               "git push": 1,
-               "git stash": 1,
-               "git add": 1,
-               "git log": 1,
-               "git bisect": 1,
-               "code reviews": 1,
-               "collaborative software development": 3,
-               "code reuse": 1,
-               "pull request": 1,
-               "github": 5,
-               "gitlab": 2, 
-               "issues/work items": 1, 
-               "sphinx": 3, 
-               ".md": 3, 
-               "MyST": 1, 
-               "test coverage": 1,
-               "conda":3,
-               "AI-assisted coding": 3,
-               "responsibility": 4,
-               "automated testing": 3,
-               "social coding": 2,
-               "containers": 3,
-               "reproducibility": 5,
-               "open science": 4,
-               "OSS licenses": 2,
-               "research integrity": 3,
-               "data repositories": 1,
-               "snakemake": 2,
-               "pytest": 1,
-               "Test.jl": 1,
-               "testthat": 1,
-               "Catch2": 1,
-               "GoogleTest": 1,
-               "github actions": 1,
-               "gitlab CI/CD": 1,
-               "python":1,
-               "R":1,
-               "C++":1,
-               "Julia":1,
-               "docker": 1,
-               "apptainer": 1,
-               "agentic AI": 1,
-               "security": 1,
-               "TDD": 1,
-               }
+weights = {"git": 10, 
+           "git commit": 1,
+           "git push": 1,
+           "git stash": 1,
+           "git add": 1,
+           "git log": 1,
+           "git bisect": 1,
+           "code reviews": 1,
+           "collaborative software development": 3,
+           "code reuse": 1,
+           "pull request": 1,
+           "github": 5,
+           "gitlab": 2, 
+           "issues/work items": 1, 
+           "sphinx": 3, 
+           ".md": 3, 
+           "MyST": 1, 
+           "test coverage": 1,
+           "conda":3,
+           "AI-assisted coding": 3,
+           "responsibility": 4,
+           "automated testing": 3,
+           "social coding": 2,
+           "containers": 3,
+           "reproducibility": 5,
+           "open science": 4,
+           "OSS licenses": 2,
+           "research integrity": 3,
+           "data repositories": 1,
+           "snakemake": 2,
+           "pytest": 1,
+           "Test.jl": 1,
+           "testthat": 1,
+           "Catch2": 1,
+           "GoogleTest": 1,
+           "github actions": 1,
+           "gitlab CI/CD": 1,
+           "python":1,
+           "R":1,
+           "C++":1,
+           "Julia":1,
+           "docker": 1,
+           "apptainer": 1,
+           "agentic AI": 1,
+           "security": 1,
+           "TDD": 1,
+           }
 
 
 if __name__ == "__main__":
